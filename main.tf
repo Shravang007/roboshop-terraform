7b7361f297b0855c910fb218e8263ff87204133e
Shravang007 <shravangoudgajula@gmail.com>
2023-06-18 18:49:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "instances" {
   source    = ""git::https://github.com/Shravang007/tf-module-app.git""
   component = each.key
 env       = var.env
-tags      = merge(each.value["tags"], var.tags)
 }
 
 
