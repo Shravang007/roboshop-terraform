a649ac78267244fba2c2205b2ba7c1c5c6d5ad9d
Shravang007 <shravangoudgajula@gmail.com>
2023-06-18 19:15:23 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "instances" {
   for_each  = var.components
   source    = "git::https://github.com/Shravang007/tf-module-app.git"
-  component = each.key
+  name = each.key
   env       = var.env
 }
 
