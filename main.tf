059e81a2f831618a76f9e8f8c290ac893c7e227e
Shravang007 <shravangoudgajula@gmail.com>
2023-07-01 23:06:34 +0530

    vpc networking

--- main.tf
+++ main.tf
@@ -9,9 +9,10 @@
 #
 
 module "vpc" {
-  for_each  = var.vpc
-  source    = "git::https://github.com/Shravang007/tf-module-vpc.git"
+  for_each   = var.vpc
+  source     = "git::https://github.com/Shravang007/tf-module-vpc.git"
   cidr_block = each.value["cidr_block"]
+}
 #  env       = var.env
 #  tags      = merge(each.value["tags"], var.tags)
 
