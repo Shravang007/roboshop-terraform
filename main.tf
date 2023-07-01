ece9a6a7dbc2f1e014b0772ab1a9cc6587b67758
Shravang007 <shravangoudgajula@gmail.com>
2023-07-01 23:02:43 +0530

    tags

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
-module "instances" {
-  for_each  = var.components
-  source    = "git::https://github.com/Shravang007/tf-module-app.git"
-  component = each.key
+#module "instances" {
+#  for_each  = var.components
+#  source    = "git::https://github.com/Shravang007/tf-module-app.git"
+#  component = each.key
+#  env       = var.env
+#  tags      = merge(each.value["tags"], var.tags)
+#}
+#
+#
+
+module "vpc" {
+  for_each  = var.vpc
+  source    = "git::https://github.com/Shravang007/tf-module-vpc.git"
+  cidr_block = each.value["cidr_block"]
   env       = var.env
   tags      = merge(each.value["tags"], var.tags)
+
 }
-
-
