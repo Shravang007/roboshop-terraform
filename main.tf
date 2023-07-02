9320fa26dc656d508a984382f799a433a0cb2ecf
Shravang007 <shravangoudgajula@gmail.com>
2023-07-02 11:06:12 +0530

    vpc networking

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "vpc" {
 
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.key["subnets"]
+  subnets = each.value["subnets"]
 
   env        = var.env
   tags       = var.tags
