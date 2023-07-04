fab80f456701f6305eb2a6e0b2c10480986a797c
Shravang007 <shravangoudgajula@gmail.com>
2023-07-04 22:32:43 +0530

    docdb

--- main.tf
+++ main.tf
@@ -23,74 +23,74 @@ module "vpc" {
 }
 
 
-#module "app_server" {
-#  source     = "git::https://github.com/Shravang007/tf-module-app.git"
-#
-#  env        = var.env
-#  tags       = var.tags
-#  component  = "test"
-#
-#  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null) [0]
-#  vpc_id     = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#
-#  #  module.vpc ["main"] ["subnet_ids"] ["app"] ["subnet_ids"] [0]
-#
-#
-#}
-#
-#module "rabbitmq" {
-#  source          = "git::https://github.com/Shravang007/tf-module-rabbitmq.git"
-#
-#  for_each        = var.rabbitmq
-#  component       = each.value["component"]
-#  instance_type   = each.value["instance_type"]
-#  sg_subnet_cidr  = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-#  vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#  subnet_id       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null) [0]
-#
-#  env            = var.env
-#  tags           = var.tags
-#  allow_ssh_cidr = var.allow_ssh_cidr
-#  zone_id        = var.zone_id
-#  kms_key_id     = var.kms_key_id
-#}
-#
-#
-#module "rds" {
-#  source = "git::https://github.com/Shravang007/tf-module-rds.git"
-#
-#
-#       for_each  = var.rds
-#  component      = each.value["component"]
-#   engine        = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  database_name  = each.value["database_name"]
-#   subnet_ids    = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-#  instance_count = each.value["instance_count"]
-#  instance_class = each.value["instance_class"]
-#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-#
-#  env           = var.env
-#   tags          = var.tags
-#   kms_key_arn   = var.kms_key_arn
-#
-#}
+module "app_server" {
+  source     = "git::https://github.com/Shravang007/tf-module-app.git"
 
-module "documentdb" {
-  source = "git::https://github.com/Shravang007/tf-module-documentdb.git"
+  env        = var.env
+  tags       = var.tags
+  component  = "test"
+
+  subnet_id  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null) [0]
+  vpc_id     = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+
+  #  module.vpc ["main"] ["subnet_ids"] ["app"] ["subnet_ids"] [0]
 
 
-  for_each       = var.documentdb
+}
+
+module "rabbitmq" {
+  source          = "git::https://github.com/Shravang007/tf-module-rabbitmq.git"
+
+  for_each        = var.rabbitmq
+  component       = each.value["component"]
+  instance_type   = each.value["instance_type"]
+  sg_subnet_cidr  = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id          = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null) [0]
+
+  env            = var.env
+  tags           = var.tags
+  allow_ssh_cidr = var.allow_ssh_cidr
+  zone_id        = var.zone_id
+  kms_key_id     = var.kms_key_id
+}
+
+
+module "rds" {
+  source = "git::https://github.com/Shravang007/tf-module-rds.git"
+
+
+       for_each  = var.rds
   component      = each.value["component"]
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+   engine        = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  database_name  = each.value["database_name"]
+   subnet_ids    = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
 
   env           = var.env
-  tags          = var.tags
-  kms_key_arn   = var.kms_key_arn
+   tags          = var.tags
+   kms_key_arn   = var.kms_key_arn
 
 }
+
+#module "documentdb" {
+#  source = "git::https://github.com/Shravang007/tf-module-documentdb.git"
+#
+#
+#  for_each       = var.documentdb
+#  component      = each.value["component"]
+#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+#
+#  env           = var.env
+#  tags          = var.tags
+#  kms_key_arn   = var.kms_key_arn
+#
+#}
 
 #module "elasticache" {
 #  source = "git::https://github.com/Shravang007/tf-module-elasticache.git"
