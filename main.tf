e4ed6c4c1ff2c8bbf4d67b901a6ac53c77936eb0
Shravang007 <shravangoudgajula@gmail.com>
2023-07-05 09:37:11 +0530

    Docdb, Elastcicache setup

--- main.tf
+++ main.tf
@@ -105,7 +105,9 @@ module "elasticache" {
   engine_version          = each.value["engine_version"]
   replicas_per_node_group = each.value["replicas_per_node_group"]
   num_node_groups         = each.value["num_node_groups"]
+  node_type               = each.value["node_type"]
   vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+
 
     env                   = var.env
     tags                  = var.tags
