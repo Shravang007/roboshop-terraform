fa2cf9293f92c08094ac8d9bd71e36bf7dc5d11a
Shravang007 <shravangoudgajula@gmail.com>
2023-07-05 08:17:37 +0530

    docdb

--- main.tf
+++ main.tf
@@ -92,19 +92,19 @@ module "documentdb" {
 
 }
 
-module "elasticache" {
-  source = "git::https://github.com/Shravang007/tf-module-elasticache.git"
-
-
-  for_each       = var.elasticache
-  component      = each.value["component"]
-
-  subnet_ids    = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-
-  env           = var.env
-  tags          = var.tags
-  kms_key_arn   = var.kms_key_arn
-
-}
+##module "elasticache" {
+##  source = "git::https://github.com/Shravang007/tf-module-elasticache.git"
+##
+##
+##  for_each       = var.elasticache
+##  component      = each.value["component"]
+##
+##  subnet_ids    = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+###  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+##  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+##
+##  env           = var.env
+##  tags          = var.tags
+##  kms_key_arn   = var.kms_key_arn
+#
+#}
