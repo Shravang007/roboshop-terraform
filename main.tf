7ea82e1c47e2b76f5518d9fa44ff10f2e96605d2
Shravang007 <shravangoudgajula@gmail.com>
2023-07-06 13:07:12 +0530

    app module setup

--- main.tf
+++ main.tf
@@ -128,17 +128,17 @@ module "vpc" {
 #
 #}
 
-#module "alb" {
-#  source = "git::https://github.com/raghudevopsb73/tf-module-alb.git"
-#
-#  for_each           = var.alb
-#  name               = each.value["name"]
-#  internal           = each.value["internal"]
-#  load_balancer_type = each.value["load_balancer_type"]
-#  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#  sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
-#  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
-#
-#  env  = var.env
-#  tags = var.tags
-#}
+module "alb" {
+  source = "git::https://github.com/raghudevopsb73/tf-module-alb.git"
+
+  for_each           = var.alb
+  name               = each.value["name"]
+  internal           = each.value["internal"]
+  load_balancer_type = each.value["load_balancer_type"]
+  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
+  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+
+  env  = var.env
+  tags = var.tags
+}
